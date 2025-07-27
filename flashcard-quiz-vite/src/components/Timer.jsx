9c4f97eade3d2a6d107e3422d5789be27aa794ac
KubranurSahin <kubranursahin@posta.mu.edu.tr>
2025-07-27 23:01:36 +0300

    Update Timer

--- flashcard-quiz-vite/src/components/Timer.jsx
+++ flashcard-quiz-vite/src/components/Timer.jsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-const Timer ({timeLeft, onTimeUp}) => {
-    const [timeLeft, setTimeLeft] = useState(timeLeft);
-    useEffect(() => {
-        if (timeLeft > 0) {
-            onTimeUp()
-            return
+const Timer = ({ initialTime, onTimeUp }) => {
+  const [timeLeft, setTimeLeft] = useState(initialTime)
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      onTimeUp()
+      return
+    }
+
+    const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000)
+    return () => clearTimeout(timerId)
+  }, [timeLeft, onTimeUp])
+
+  const minutes = Math.floor(timeLeft / 60)
+  const seconds = timeLeft % 60
+
+  return (
+    <div className="timer">
+      <h3>Kalan Süre: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h3>
+    </div>
+  )
 }
 
-const timerId = setTimeout(() => 
-    setTimeLeft(timeLeft - 1), 1000); // 1 saniyede bir güncelle
-        return () => clearTimeout(timerId); // Temizleme işlemi
-    }, [timeLeft, onTimeUp]);
-
-    const minutes = Math.floor(timeLeft / 60); // Dakikaları hesapla
-    const seconds = timeLeft % 60; // Saniyeleri hesapla    
-
-    return (
-        <div className="timer">
-            <h3>Kalan Süre: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-            </h3>
-        </div>
-    )
-
-}
-export default Timer;
+export default Timer

e60a004dbfdf935e3c7d78137a6298d04c126db1
KubranurSahin <kubranursahin@posta.mu.edu.tr>
2025-07-27 18:13:34 +0300

    update

--- flashcard-quiz-vite/src/App.jsx
+++ flashcard-quiz-vite/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState, useEffect } from "react";
 import {format} from "date-fns/format";
 import UserForm from "./components/UserForm";
-import Quiz from "./components/Quiz";
 import { questions } from "./data/questions";
 import History from "./components/History";
 import Timer from "./components/Timer";
 import QuestionCard from "./components/QuestionCard";
-import Result from "./components/Result";
 import ResultModal from "./components/ResultModal";
 
-import "./styles.css";
-
+import './App.css';
 function App() {
   const [userName, setUserName] = useState("");
   const [quizStarted, setQuizStarted] = useState(false); // Quiz'in başlatılıp başlatılmadığını kontrol eder
